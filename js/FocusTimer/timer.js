e7cc3ec8bff176a6c05056907a3f135457c80977
VickyBentoo <99763397+VickyBentoo@users.noreply.github.com>
2024-06-15 01:33:13 -0300

    Add files via upload

---
+++ js/FocusTimer/timer.js
@@ -0,0 +1,12 @@
+import state from './state.js'
+import * as element from './elements.js'
+
+
+export function updateDisplay(minutes, seconds) {
+    minutes = minutes ?? state.minutes /*nullish coalesing operator */
+    seconds = seconds ?? state.seconds
+    
+    
+    element.minutes.textContent = String(minutes).padStart(2, "0")
+    element.seconds.textContent = String(seconds).padStart(2, "0")
+}
